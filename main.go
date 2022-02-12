c4a4e22c36b5e09a56a8de523494e8a412b77442
Harry Coa <rodol_2000@hotmail.com>
2022-02-12 10:48:01 -0500

    feat:[Servidor] - Creacion del servidor y prueba de una peticion

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+)
 
-func main(){
-	fmt.Println("=============>")
+const (
+	GET  string = "GET"
+	POST string = "POST"
+)
+
+func main() {
+	fmt.Println("Servidor")
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Method == GET {
+			json.NewEncoder(w).Encode(" Funciona")
+		}
+		if r.Method == POST {
+			json.NewEncoder(w).Encode(" Funciona POST")
+		}
+
+	})
+
+	// Servidor
+	server := http.Server{
+
+		Addr:              ":4000",
+		Handler:           router,
+		ReadTimeout:       30 * time.Second,
+		ReadHeaderTimeout: 30 * time.Second,
+		WriteTimeout:      30 * time.Second,
+		IdleTimeout:       30 * time.Second,
+	}
+
+	server.ListenAndServe()
+
 }
